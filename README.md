77ea558f570b724879177e93b0bb79f815dda0e6
sruthisivakumar16 <sru.s316@gmail.com>
2020-10-12 13:35:28 +0530

    inital commit

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Snake-2D
+
+A simple 2D game that takes you right back to your childhood when all you needed was a simple Nokia 1100 to feel good.
+
+You can play this game at : https://sruthisivakumar16.github.io/Snake-2D.github.io/
+
+Happy slithering!
+
+## SCREENSHOT
+
+<img src = "screenshot.png"/>
+
+
---
+++ food.js
@@ -0,0 +1,32 @@
+import {onSnake,expandSnake} from './snake.js'
+import {randomGridPosition} from './grid.js'
+const EXPANSION_RATE = 3
+let food = getRandomFoodPosition()
+
+export function update() { 
+    
+    if (onSnake(food)) {
+        expandSnake(EXPANSION_RATE)
+        food = getRandomFoodPosition()
+    }
+}
+
+export function draw(gameBoard) {
+        const foodElement = document.createElement('div')
+        foodElement.style.gridRowStart = food.y
+        foodElement.style.gridColumnStart = food.x
+        foodElement.classList.add('food')
+        gameBoard.appendChild(foodElement)
+        
+        
+    }
+
+function getRandomFoodPosition() {
+    let newFoodPosition
+    while( newFoodPosition == null || onSnake(newFoodPosition)){
+        newFoodPosition = randomGridPosition()
+    }
+    return newFoodPosition
+}
+
+    
